08952eb590138ddb3d668ff214a2a507cd49e550
SEMENOV Andrey V <andrey.semenov@raiffeisen.ru>
2024-10-24 20:54:31 +0300

    Add proxy configurator

--- src/main/java/spring/deserve/it/game/LogProxyConfigurator.java
+++ src/main/java/spring/deserve/it/game/LogProxyConfigurator.java
@@ -1,5 +1,7 @@
 package spring.deserve.it.game;
 
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.InvocationHandler;
 import org.reflections.ReflectionUtils;
 
 import java.lang.reflect.Method;
@@ -26,20 +28,20 @@ public class LogProxyConfigurator implements ProxyConfigurator {
                 Object proxy = newProxyInstance(implClass.getClassLoader(), implClass.getInterfaces(), (o, method, args) -> {
                     if (methodNameToLoggedParameters.containsKey(method.getName())) {
                         String[] loggedParameters = methodNameToLoggedParameters.get(method.getName());
-                        Map<String, Integer> parametersByIndexInSignature = new HashMap<>();
-                        Integer counter = 0;
-                        for (Parameter parameter : method.getParameters()) {
-                            parametersByIndexInSignature.put(parameter.getName(), counter);
-                            counter++;
-                        }
-                        Map<String, Integer> argsNameToIndex = new HashMap<>();
-                        for (String loggedParameter : loggedParameters) {
-                            if (parametersByIndexInSignature.containsKey(loggedParameter)) {
-                                argsNameToIndex.put(loggedParameter, parametersByIndexInSignature.get(loggedParameter));
-                            } else {
-                                throw new RuntimeException("Invalid paraeter name for logging");
-                            }
-                        }
+                        Map<String, Integer> argsNameToIndex = getArgsNameToIndex(method, loggedParameters);
+                        String argsString = argsNameToIndex.entrySet().stream().map(e -> e.getKey() + " = " +
+                                args[e.getValue()]).collect(Collectors.joining(","));
+                        System.out.println("Invoke method " + method.getName() + " " + argsString);
+                        return method.invoke(originalObject, args);
+                    }
+                    return method.invoke(originalObject, args);
+                });
+                return proxy;
+            } else {
+                Object proxy = Enhancer.create(implClass, (InvocationHandler) (o, method, args) -> {
+                    if (methodNameToLoggedParameters.containsKey(method.getName())) {
+                        String[] loggedParameters = methodNameToLoggedParameters.get(method.getName());
+                        Map<String, Integer> argsNameToIndex = getArgsNameToIndex(method, loggedParameters);
                         String argsString = argsNameToIndex.entrySet().stream().map(e -> e.getKey() + " = " +
                                 args[e.getValue()]).collect(Collectors.joining(","));
                         System.out.println("Invoke method " + method.getName() + " " + argsString);
@@ -52,5 +54,23 @@ public class LogProxyConfigurator implements ProxyConfigurator {
         }
 
         return originalObject;
+    }
+
+    private static Map<String, Integer> getArgsNameToIndex(Method method, String[] loggedParameters) {
+        Map<String, Integer> parametersByIndexInSignature = new HashMap<>();
+        Integer counter = 0;
+        for (Parameter parameter : method.getParameters()) {
+            parametersByIndexInSignature.put(parameter.getName(), counter);
+            counter++;
+        }
+        Map<String, Integer> argsNameToIndex = new HashMap<>();
+        for (String loggedParameter : loggedParameters) {
+            if (parametersByIndexInSignature.containsKey(loggedParameter)) {
+                argsNameToIndex.put(loggedParameter, parametersByIndexInSignature.get(loggedParameter));
+            } else {
+                throw new RuntimeException("Invalid paraeter name for logging");
+            }
+        }
+        return argsNameToIndex;
     }
 }
