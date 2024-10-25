82fd8ef6e157e4d5f7a65e7289401942f22835ce
ruacheo <Ekaterina.CHEREPANOVA@raiffeisen.ru>
2024-10-25 10:18:14 +0300

    homework

--- src/main/java/spring/deserve/it/game/ObjectFactory.java
+++ src/main/java/spring/deserve/it/game/ObjectFactory.java
@@ -1,32 +1,17 @@
 package spring.deserve.it.game;
 
 import jakarta.annotation.PostConstruct;
-import lombok.SneakyThrows;
-import org.reflections.*;
-
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
-
 import lombok.SneakyThrows;
-import org.reflections.Reflections;
-
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import lombok.SneakyThrows;
-import org.reflections.Reflections;
-
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import lombok.SneakyThrows;
-import org.reflections.Reflections;
-
-import java.util.Set;
-import java.util.stream.Collectors;
-
+import org.reflections.ReflectionUtils;
 import static org.reflections.ReflectionUtils.withAnnotation;
+import org.reflections.Reflections;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
 
 public class ObjectFactory {
 
@@ -56,12 +41,46 @@ public class ObjectFactory {
 
         // Применяем все конфигураторы, передавая им контекст
         for (ObjectConfigurator configurator : configurators) {
-          // Передаем контекст через сеттер
+            // Передаем контекст через сеттер
             configurator.configure(obj);
         }
         invokePostConstruct(obj);
+        if (ReflectionUtils.getAllMethods(obj.getClass(), withAnnotation(Log.class)).size() > 0) {
+            obj = (T) createProxyObject(obj);
+        }
 
         return obj;
+    }
+
+    @SneakyThrows
+    private <T> Object createProxyObject(T object) {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(object.getClass());
+        enhancer.setCallback((MethodInterceptor) (obj, method, args, proxy) -> {
+            if (Arrays.stream(method.getAnnotations()).anyMatch(annotation -> annotation instanceof Log)) {
+                Log annotation = method.getAnnotation(Log.class);
+                String propertyToLog = annotation.value();
+                Object valueToLog = getValueToLog(obj, propertyToLog);
+                System.out.println(String.format("Logged %s : %s = %s", obj.getClass(), propertyToLog, valueToLog));
+            }
+            return proxy.invokeSuper(obj, args);
+        });
+        T proxy = (T) enhancer.create();
+        return proxy;
+    }
+
+    private Object getValueToLog(Object obj, String propertyToLog) throws IllegalAccessException {
+        Field field = getField(obj, propertyToLog);
+        field.setAccessible(true);
+        return field.get(obj);
+    }
+
+    private Field getField(Object obj, String propertyToLog) {
+        return ReflectionUtils.getAllFields(obj.getClass())
+                .stream()
+                .filter(f -> f.getName().equals(propertyToLog))
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
     }
 
     @SneakyThrows
