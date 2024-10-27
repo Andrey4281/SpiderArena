f81a822b4dc7c59be4c893cc2aa2f6ccf308d6a3
SEMENOV Andrey V <andrey.semenov@raiffeisen.ru>
2024-10-27 14:41:30 +0300

    Homework 2. Third additional task

---
+++ src/main/java/spring/deserve/it/infra/SpiderLifeAspect.java
@@ -0,0 +1,32 @@
+package spring.deserve.it.infra;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import spring.deserve.it.api.Spider;
+
+import java.util.List;
+
+@Aspect
+@Component
+public class SpiderLifeAspect {
+
+    private final List<String> pattern = List.of("Paper");
+
+    @Around("execution( * spring.deserve.it.api.Spider.loseLife())")
+    public Object aroundLoseLife(ProceedingJoinPoint joinPoint) throws Throwable {
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        Object result = joinPoint.proceed();
+
+        boolean isImmortalSpider = pattern.stream().anyMatch(className::contains);
+        if (isImmortalSpider) {
+            Spider target = (Spider) joinPoint.getTarget();
+            if (target.getLives() == 1) {
+                target.setLives(10);
+            }
+        }
+        return result;
+    }
+
+}
